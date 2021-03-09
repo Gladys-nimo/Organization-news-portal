028e8cf7f4fc0cf09d87d3359731c5749790b919
Gladys-nimo <wkanyora10@gmail.com>
2021-03-10 01:03:06 +0300

    added an apiexception class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,9 @@ import models.Users;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -158,6 +160,21 @@ public class App {
 
             response.status(201);
             return gson.toJson(departmentUsers);
+        });
+
+        exception(ApiException.class, (exception, request, response) -> {
+            ApiException err = exception;
+            Map<String, Object> jsonMap = new HashMap<>();
+            jsonMap.put("status", err.getStatusCode());
+            jsonMap.put("errorMessage", err.getMessage());
+            response.type("application/json");
+            response.status(err.getStatusCode());
+            response.body(gson.toJson(jsonMap));
+        });
+
+
+        after((request, response) ->{
+            response.type("application/json");
         });
     }
 
